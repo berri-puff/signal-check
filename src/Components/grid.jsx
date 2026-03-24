cb8e87f87cde6c942f4e6bd678e632ccdb2329bc
Jenny Zhang <jennyzhang0207@gmail.com>
2026-03-24 16:49:13 +0000

    Add basic functionality for finding valid chains

--- src/Components/grid.jsx
+++ src/Components/grid.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useEffect, useState } from "react"
+import { useMemo, useEffect, useState, startTransition } from "react"
 import Cell from "./cell";
 
 const Grid = ( {processedSignals} ) => {
@@ -29,13 +29,7 @@ const Grid = ( {processedSignals} ) => {
     
     useMemo(()=> {
       const calculatePeaks = (row, column, peakCoordinates) => {
-        // check the neighbouring coordiantes are increment of itself 
-        // Part 1 Calculation here, distinct 9's keeping count
-        /*
-            take in coordinate of the peak, if it's a repeat, keep the score as one
-            else add the coordinates to the the peaksCoordinates set, and add 1 
-        */
-        //    check that either side of the direction of the starting value can be travelled 
+        // calculate reachable peaks and store it 
         if (grid[row][column] === 9) {
           peakCoordinates.add(`${row},${column}`)
           return
@@ -44,8 +38,6 @@ const Grid = ( {processedSignals} ) => {
         for (const [dirRow, dirCol] of directions) {
           const newRow = row + dirRow
           const newColumn = column + dirCol
-
-          // Check that it is manuvable row and column wise and it is more than current value
 
           if (newRow >= 0 && newRow < rowCount && newColumn >= 0 && newColumn < columnCount) {
               if (grid[newRow][newColumn] === grid[row][column] + 1) {
@@ -56,25 +48,36 @@ const Grid = ( {processedSignals} ) => {
       }
 
       const calculateSignalChains = (row, column) =>{
-        // Part 2 calculation, working out the total distinct paths to 9 
-        // another new set to save the coordinates to get to the 
-  
+        // store the signal chains for a specific 0 
+        if (grid[row][column] === 9) return 1;
+        
+        let paths = 0
+
+        for (const [dirRow, dirCol] of directions) {
+          const newRow = row + dirRow
+          const newColumn = column + dirCol
+
+          if (newRow >= 0 && newRow < rowCount && newColumn >= 0 && newColumn < columnCount) {
+              if (grid[newRow][newColumn] === grid[row][column] + 1) {
+                paths += calculateSignalChains(newRow,newColumn)
+              }
+          }
+        }
+        return paths
       }
 
       for (let row = 0; row < rowCount; row++) {
         for (let column = 0; column < columnCount; column++) {
             if (grid[row][column] === 0) {
-              // call find peaks 
-              // call find signal chains 
               const peakCoordinates = new Set()
               calculatePeaks(row, column, peakCoordinates)
-              // New set for distinct chains for coordinates for travel through
-              calculateSignalChains(row, column)
+              peaksFound += peakCoordinates.size
+              // store theses values against the current [row][column]
+              signalChains += calculateSignalChains(row, column)
 
             }
         }
       }
-      console.log(peaksFound, "found")
     }, [grid, rowCount, columnCount])
 
   return(
@@ -104,7 +107,7 @@ const Grid = ( {processedSignals} ) => {
     <div>
         <h3>Results</h3>
         <h4>Peaks: {peaksFound}</h4>
-        <h4>Distinct Chains: </h4>
+        <h4>Distinct Chains: {signalChains}</h4>
     </div>
     </div>
   )
