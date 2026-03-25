7071076abe12c60b4ea0f27b88d0ab953081f6b9
Jenny Zhang <jennyzhang0207@gmail.com>
2026-03-25 17:02:24 +0000

    Add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import Grid from './Components/grid';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
   const maxCount = 60 // Maximum number of cells for both rows and columns
@@ -8,19 +9,30 @@ function App() {
   const [signalValues, setSignalValues] = useState("");
   const [processedSignals, setProcessedSignals] = useState([])
   const [generateGrid, setGenerateGrid] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+  const ref = useRef(null);
 
   const handleTextChange = (event) => {
     setSignalValues(event.target.value);
   };
+
+ const showError = (msg) => {                                                                                                                                   
+    setErrorMessage(msg);                                                                                                                                                                                                                                                         
+    setTimeout(() => {                                                                                                                                           
+      setErrorMessage("");                                                                                                                                       
+    }, 3000);                                                                                                                                                    
+  };                                                                                                                                                             
+
 
   const isValidInput = (signals) => {
     const numbersRegex = /^[0-9]+$/
     const onlyNumbers = signals.every((signal) => numbersRegex.test(signal))
     const isSameLength = signals.every((signal) => signal.length === signals[0].length)
     const isAboveMax = signals.length > maxCount && signals[0].length > maxCount
+
     
     if (!onlyNumbers || !isSameLength || isAboveMax){
-      console.log("display error feedback here, only numbers are allowed")
+      !isSameLength ? showError("Please ensure all numbers are of equal length") : showError("Only digitals between 1 and 60, no letters and special characters")
       return false
     }
     else {
@@ -29,7 +41,9 @@ function App() {
   }
 
   const processSignalInputs = () => {
-    // const signalValueArray = signalValues.trim().split("\n").replaceAll(/\s/g,'')
+    if (signalValues.length === 0) {
+      showError("No empty strings are allowed")
+    }
     const signalValueArray = signalValues.split("\n").map(line => line.trim().replace(/\s+/g, '')).filter(line => line.length > 0); 
     if (isValidInput(signalValueArray)) {
       const newInput = signalValueArray.map(row => row.split('').map(Number));
@@ -40,10 +54,22 @@ function App() {
 
   return (
     <div>
+      <CSSTransition
+        in={errorMessage.length > 0}
+        classNames="error-container"
+        nodeRef={ref}
+        timeout={300}
+        unmountOnExit
+      >
+        <div ref={ref} className="error-container">
+         <p className="error-message">{errorMessage}</p> 
+        </div>
+      </CSSTransition>
+      
       <h1 className="main-header">Enter Signal Values</h1>
       <p>Please enter only digits between 1 and 60</p>
       <div className="signal-input-container">
-        <textarea className="signal-input-area" rows={8} cols={60} values={signalValues} onChange={(e) => {handleTextChange(e)}} />
+        <textarea className="signal-input-area" rows={8} cols={60} value={signalValues} onChange={(e) => {handleTextChange(e)}} />
         <button className="button" onClick={() => {processSignalInputs()}}>Confirm</button>
       </div>
 
